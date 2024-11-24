c8849c333409867d1fb7eece955fea34890d08b1
techshomez <danieltopeg@gmail.com>
2024-11-24 20:01:08 +0000

    search dropdown component

--- app/components/common/header/index.js
+++ app/components/common/header/index.js
@@ -5,13 +5,25 @@ import Link from "next/link";
 import { HiChevronDown,HiOutlineLogin  } from "react-icons/hi";
 import { menuData } from "../../utilities/menuData";
 
+import { useState } from "react";
+
 function Header() {
+
+  const [searchDropdown, setSearchDropdown] = useState(false);
+
+  const handleSearchDropdown = ()=>{
+    setSearchDropdown(!searchDropdown);
+  }
+
+
   return (
     <>
     <header className="header">
       <div className="container">
         <div className="logo">
+          <Link href="/">
           <Image src="/images/logo.png" alt="Logo" width={100} height={30} />
+          </Link>
         </div>
         <div className="middle__nav">
           {menuData.map((menu_items, index)=>{
@@ -30,8 +42,25 @@ function Header() {
             </div>
             <div className="divider"></div>
             <div className="search__dropdown">
-              <span>Talent</span>
+              <div className="search__holder">
+              <span onClick={handleSearchDropdown}>Talent</span>
               <i><HiChevronDown /></i>
+              </div>
+              {searchDropdown &&(
+                <div className="search__dropdown__item">
+                <div className="search__dropdown__item_holder">
+                  <div className="search__dropdown__item_list">
+                    <div className="search__dropdown__item_left">
+                      <i></i>
+                    </div>
+                    <div className="search__dropdown__item_right">
+                      <span>Talent</span>
+                      <small>Hire professional</small>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            )}
             </div>
           </div>
           <div className="nav__cta">
